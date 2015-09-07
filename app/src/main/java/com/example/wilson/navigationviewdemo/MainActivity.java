a662e256f2ef04816378a283bc17b588bb3070bc
princegyw <yiwei.gu09@gmail.com>
2015-09-07 23:34:48 +0800

    add

--- app/src/main/java/com/example/wilson/navigationviewdemo/MainActivity.java
+++ app/src/main/java/com/example/wilson/navigationviewdemo/MainActivity.java
@@ -1,37 +1,62 @@
 package com.example.wilson.navigationviewdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener{
+
+    private NavigationView mNavigationView;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mNavigationView = (NavigationView) findViewById(R.id.id_nv_menu);
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.id_toolbar);
+        toolbar.setTitle("Home");
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.actionbar_nav_48); //set Home button Icon
+            actionBar.setDisplayHomeAsUpEnabled(true); //display Home button
+        }
+
     }
 
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+        if (menuItem.getItemId() == R.id.nav_home) {
+
+            Log.d("gyw", "MainActivity::onNavigationItemSelected home selected");
+        }
+        menuItem.setChecked(true);
+
         return true;
     }
 
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(item.getItemId() == android.R.id.home)
+        {
+            mDrawerLayout.openDrawer(GravityCompat.START);
         }
-
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 }
